38b31a28ca779f34e19ae586ff4279caf1394356
Taofeek Olusola <olusolataofeek@gmail.com>
2023-10-31 18:58:37 +0100

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    if (i == 101) or(I == 113):
+    if (a == 101 or 113):
         continue
-    print(chr(i).format(), end="")
+    print(chr(a).format(), end="")
