477d85b10cd614629479e98d8fa54b7b19aa6cdf
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-02 21:20:23 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
 
     if size > 1:
         print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
+        for a in range(1, size + 1):
+            print("{}: {}".format(a, arg[a]))
 
     elif size == 0:
         print("{} arguments.".format(size))
