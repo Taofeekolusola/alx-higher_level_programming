d31f4832793bc47c3d2aab1d3f3db5797002560f
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-06 18:14:42 +0100

    0x03-python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
-    for i in range(len(matrix)):
-        for nums in range(len(matrix[i])):
-            if nums != len(matrix[i])-1:
-                last_space = ''
-            else:
-                last_space = ""
-            print("{:d}".format(matrix[i][nums]), end=last_space)
-        print()
+    else:
+        for i in range(len(matrix)):
+            for nums in range(len(matrix[i])):
+                if nums != len(matrix[i])-1:
+                    last_space = ''
+                else:
+                    last_space = ""
+                print("{:d}".format(matrix[i][nums]), end=last_space)
+            print()
