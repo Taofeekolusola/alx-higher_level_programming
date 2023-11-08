0701097f2a083ff1f213e65ce429f4e4e6b34b86
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-08 17:37:53 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = list(my_list)
     for a in range(len(my_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
+        if new_list[a] == search:
+            new_list[a] = replace
     return new_list
