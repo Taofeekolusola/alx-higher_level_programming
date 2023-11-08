9f0dbee8016e7a00009d9edafcb59b192df50cd5
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-08 17:24:13 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
 
-    new_matrix = list(map((lambda x: x ** 2), matrix))
+    new_matrix = [[a ** 2 for x in ] for x in matrix]
     return new_matrix
