c2cc0aa24f3f5c9f4599c5ad555063a21dc4cc1a
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-08 19:21:09 +0100

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,14 @@
 def roman_to_int(roman_string: str):
     if roman_string is None or type(roman_string) != str:
         return 0
-    items = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = [items[a] for a in roman_string] + [0]
-    counter = 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
-    for b in range(len(num) - 1):
-        if numb[b] >= num[b+1]:
-            counter += num[b]
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            counter -= numbers[b]
+            rep -= numbers[i]
 
-    return counter
+    return rep
