30b4110b883dea346f7758e1cf5bfa8fc70e2cee
Taofeek Olusola <olusolataofeek@gmail.com>
2023-11-20 18:01:40 +0100

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        res = a / b
-    except ZeroDivisionError:
-        print("Error: Division by zero")
-        return None
+       res = a / b
+    except (ZeroDivisionError):
+        res = None
     finally:
-        print("inside result = {}".format(res))
+        print("Inside result: {}".format(res))
         return res
