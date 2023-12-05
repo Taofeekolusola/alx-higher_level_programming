725552621ab1b157815e9d692540ebbf05337253
Taofeek Olusola <olusolataofeek@gmail.com>
2023-12-05 22:59:01 +0100

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Starts a new Student.
+
+        Args:
+            first_name (str): Student first name.
+            last_name (str): Student last name.
+            age (int): Student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(e) == str for e in attrs)):
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for key, val in json.items():
+            setattr(self, key, val)
