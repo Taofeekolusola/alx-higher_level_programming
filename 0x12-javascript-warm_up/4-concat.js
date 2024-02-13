3e2b0da1d74a51779eebba0621a52335f3e06cc6
Taofeek Olusola <olusolataofeek@gmail.com>
2024-02-13 07:41:58 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,8 @@ const a = process.argv[2];
 const b = process.argv[3];
 if (process.argv.length > 3) {
   console.log(a + ' is ' + b);
+} else if (process.argv.length > 2) {
+  console.log(a + ' is ' + 'undefined');
+} else {
+  console.log('undefined' + ' is ' + 'undefined');
 }
