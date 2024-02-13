ac6761f5e0b4b419a2795b9b6b30c1797808aed8
Taofeek Olusola <olusolataofeek@gmail.com>
2024-02-13 18:56:25 +0100

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function fact(a) {
+  if (isNaN(a) || a === 0) {
+    return 1;
+  } else {
+    return a * fact(a - 1);
+  }
+}
+
+if (process.argv.length === 3) {
+  const inputNumber = parseInt(process.argv[2]);
+  console.log(fact(inputNumber));
+}
