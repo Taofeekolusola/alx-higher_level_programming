8f9ae916c939277415b7e1d7502fa451a432e960
Taofeek Olusola <olusolataofeek@gmail.com>
2024-02-14 22:09:01 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const numbers = args.map(arg => parseInt(arg));
+const validNumbers = numbers.filter(num => !isNaN(num));
 
+if (validNumbers.length >= 2) {
+  const sortedNumbers = validNumbers.sort((a, b) => b - a);
+  const secondHighestNumber = sortedNumbers[1];
+  console.log(secondHighestNumber);
+} else if (validNumbers.length === 1) {
+  console.log(0);
+} else {
+  console.log(0);
+}
